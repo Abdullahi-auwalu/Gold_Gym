09cee7107344a2d5874762c2a02ee332fe594010
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-08-24 11:50:24 +0100

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
 
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
+
 
 function App() {
 
   return (
-    <div>
-      App
-    </div>
+    <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path="/exercises/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
