7f5a7af6a10a42fae5085bb549080820ef718b43
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-09-05 01:15:59 +0100

    add exercise to exersice page

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -9,16 +9,19 @@ import SimilarExercises from "../components/SimilarExercises"
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({})
-  const { id } = useParams
+  const { id } = useParams()
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
 
-      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`,exerciseOptions)
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${ id }`, exerciseOptions)
+      console.log(exerciseDetailData);
       setExerciseDetail(exerciseDetailData)
     }
+
+    fetchExercisesData()
   },[id]) 
 
   return (
