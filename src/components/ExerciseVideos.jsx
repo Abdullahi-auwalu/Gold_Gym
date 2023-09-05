69cf8b1ca2059f0bd0e9b553e7e23a576428852e
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-09-05 04:11:14 +0100

    add some typography to exercise video page

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -21,14 +21,14 @@ const ExerciseVideos = ({exerciseVideos, name}) => {
       </Typography>
       <Stack 
         justifyContent="flex-start" 
-        // flexWrap="wrap"
+        flexWrap="wrap"
         alignItems="center"
         sx={{
           flexDirection: {lg: 'row'},
           gap: {lg: "110px", xs: "0"}
         }}
       >
-        {exerciseVideos?.slice(0, 3).map((item, index) => (
+        {exerciseVideos?.slice(0, 6).map((item, index) => (
           <a 
             key={index}
             className="exercise-video"
@@ -36,7 +36,21 @@ const ExerciseVideos = ({exerciseVideos, name}) => {
             target="_blank"
             rel="noreferrer"
           >
-            <img src={item.video.thumbnails[0].url} alt={item.video.tittle} />
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <Box>
+              <Typography 
+                variant="h5"
+                color="#000"
+              >
+                {item.video.title}
+              </Typography>
+              <Typography 
+                variant="h6"
+                color="#000"
+              >
+                {item.video.channelName}
+              </Typography>
+            </Box>
           </a>
         ))}
       </Stack>
