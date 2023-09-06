decd844ce862bff22481122d133dff40500be296
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-09-06 02:13:25 +0100

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,27 @@
+import { Box, Stack, Typography } from "@mui/material"
+import Logo from "../assets/images/Logo-1.png"
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <Box 
+      mt="80px"
+      bgcolor="#fff3f4"
+    >
+      <Stack 
+        gap="40px"
+        alignItems="center"
+        px="40px"
+        pt="24px"
+      >
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h6">
+          Made with ❤️ by Abdullahi
+        </Typography>
+        <Typography variant="h6">
+          &copy; {new Date().getFullYear()}
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
